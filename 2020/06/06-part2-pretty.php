a3c5038e1befd8c480f66b7c0e27807197b6b6ee
theschitz <theodore@theschitz.se>
2020-12-06 11:50:04 +0100

    ok I could remove a few more lines ;)

--- 2020/06/06-part2-pretty.php
+++ 2020/06/06-part2-pretty.php
@@ -1,10 +1,6 @@
 <?php
 $groups = explode("\n\n", file_get_contents('input.txt'));
 
-function filter_by_group_size(array $group_answers, int $group_size): array {
-    return array_filter($group_answers, function($k) use ($group_size) { return $k === $group_size; });
-}
-
 $answers_sums = [];
 foreach ($groups as $group) {
     $group_answers = [];
@@ -14,7 +10,7 @@ foreach ($groups as $group) {
             $group_answers[$c] = array_key_exists($c, $group_answers) ? $group_answers[$c] + 1 : 1;
         }
     }
-    $ans = filter_by_group_size($group_answers, sizeof($group_members));
+    $ans = array_filter($group_answers, function($k) use ($group_members) { return $k === sizeof($group_members); });
     array_push($answers_sums, sizeof($ans));
 }
 
