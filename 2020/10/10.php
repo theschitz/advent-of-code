8d017ea20db0606f58264c8e79d723af5d13cb74
theschitz <theodore@theschitz.se>
2021-08-10 17:44:40 +0200

    day 10 part 2

--- 2020/10/10.php
+++ 2020/10/10.php
@@ -1,12 +1,13 @@
 <?php
 $input = explode("\n", file_get_contents('input.txt'));
 #$input = explode("\n", file_get_contents('test-input.txt'));
+array_push($input, 0);
+array_push($input, max($input) + 3);
+sort($input);
 
 function number_of_differences($adapters, $preceding_joltage = 0): array
 {
-    $device_joltage = (int)max($adapters) + 3;
     $diff_count = [1 => 0, 3 => 0];
-    sort($adapters);
     for ($i = 0; $i < sizeof($adapters); $i++) {
         $diff = (int)$adapters[$i] - $preceding_joltage;
         if ($diff === 1) {
@@ -17,9 +18,30 @@ function number_of_differences($adapters, $preceding_joltage = 0): array
         }
         $preceding_joltage = (int)$adapters[$i];
     }
-    $diff_count[3]++; // device
     return $diff_count;
+}
+
+function distinct_arrangements($adapters): int
+{
+    $output = (int)max($adapters);
+    $distinct = array_fill(0, $output + 1, null);
+    $distinct[0] = 1;
+    if (in_array(1, $adapters)) {
+        $distinct[1] = 1;
+    }
+    if (in_array(2, $adapters) && in_array(1, $adapters)) {
+        $distinct[2] = 2;
+    }
+
+    for ($i = 3; $i < $output + 1; $i++) {
+        if (!in_array($i, $adapters)) {
+            continue;
+        }
+        $distinct[$i] = $distinct[$i - 3] + $distinct[$i - 2] + $distinct[$i - 1];
+    }
+    return $distinct[$output];
 }
 
 $diff = number_of_differences($input, $outlet_joltage);
 echo "Part 1 ==> " . $diff[1] * $diff[3] . PHP_EOL;
+echo "Part 2 ==> " . distinct_arrangements($input) . PHP_EOL;
