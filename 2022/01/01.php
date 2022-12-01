2a6abf2cc990071e39a2303f21d46feadb2f6b50
theschitz <17023248+theschitz@users.noreply.github.com>
2022-12-01 08:27:53 +0100

    day 1

---
+++ 2022/01/01.php
@@ -0,0 +1,24 @@
+<?php
+
+$input = file_get_contents(__DIR__ . '/input.txt');
+// $input = file_get_contents(__DIR__ . '/test-input.txt');
+
+$input = explode(PHP_EOL.PHP_EOL, $input);
+$data = [];
+$data = array_map(
+    function ($i) {
+        return array_sum(explode(PHP_EOL, $i));
+    },
+    $input
+);
+
+$result = array_keys($data, max($data))[0] + 1;
+echo sprintf(
+    "The elf carring the most calories is #%s, carrying %s calories.%s",
+    $result, max($data), PHP_EOL
+);
+rsort($data);
+echo sprintf(
+    "The top three elves carrying the most calories are carrying %s in total.%s",
+    $data[0] + $data[1] + $data[2], PHP_EOL
+);
