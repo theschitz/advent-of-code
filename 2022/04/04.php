37d72a14f65970ff06876c49046c4fb74f8bbd8a
theschitz <17023248+theschitz@users.noreply.github.com>
2022-12-04 07:11:30 +0100

    day 04 part 01 & 02

--- 2022/04/04.php
+++ 2022/04/04.php
@@ -1,4 +1,16 @@
 <?php
 
-$input = file_get_contents(__DIR__ . '/input.txt');
-// $input = file_get_contents(__DIR__ . '/test-input.txt');
+$input = explode(PHP_EOL, file_get_contents(__DIR__ . '/' . (isset($argv[1]) ? 'test-' : ''). 'input.txt'));
+
+$input = array_map(function ($i) { return [explode('-', explode(',', $i)[0]), explode('-', explode(',', $i)[1])];}, $input);
+$fullyOverlapped = 0;
+$overlapped = 0;
+foreach ($input as $sections) {
+    $a = range($sections[0][0], $sections[0][1]);
+    $b = range($sections[1][0], $sections[1][1]);
+    $fullyOverlapped += (empty(array_diff($a, $b)) || empty(array_diff($b, $a))) ? 1 : 0;
+    $overlapped += count(array_unique(array_merge($a, $b))) < (count($a) + count($b)) ? 1 : 0;
+}
+
+echo "Part 01: Fully overlapped assignment pairs: $fullyOverlapped." . PHP_EOL; // 644
+echo "Part 02: Overlapping pairs: $overlapped." . PHP_EOL; // 926
