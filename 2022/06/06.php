9236e33ffa243b2c000014065bbbad09589ade3a
theschitz <17023248+theschitz@users.noreply.github.com>
2022-12-06 07:42:16 +0100

    day 06 part 01 and 02

--- 2022/06/06.php
+++ 2022/06/06.php
@@ -2,4 +2,17 @@
 
 declare(strict_types=1);
 
-$input = explode(PHP_EOL, file_get_contents(__DIR__ . '/' . (isset($argv[1]) ? 'test-' : ''). 'input.txt'));
+$input = trim(file_get_contents(__DIR__ . '/' . (isset($argv[1]) ? 'test-' : ''). 'input.txt'));
+
+function startOfPacket(string $string, int $lookAhead = 4): int
+{
+    for ($i=0; $i < strlen($string); $i++) {
+        if (count(array_unique(str_split(substr($string, $i, $lookAhead)))) === $lookAhead) {
+            return $i + $lookAhead;
+        }
+    }
+    return 0;
+}
+
+echo "Part 01: Characters needed to be processed before the first start-of-packet: " . startOfPacket($input) . PHP_EOL; // 1544
+echo "Part 02: Characters needed to be processed before the first start-of-message: " . startOfPacket($input, 14) . PHP_EOL; // 2145
